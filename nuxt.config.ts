591540fd96862f8d1fe740c08700d490e8d86ef4
AIYA861202 <phiphiphi27@gmail.com>
2025-10-10 17:34:31 +0800

    部屬github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+   ssr: true, // 開啟 SSR 模式
+  nitro: {
+    preset: 'github-pages' // 讓 Nuxt 產出 GitHub Pages 相容的靜態檔案
+  },
+  app: {
+    baseURL: '/nuxt-weather/', // 👈 一定要加上你的 repo 名稱（結尾有斜線）
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
