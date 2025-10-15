a3e2c228b863e53d9ce5cb0a5ad67b678a34cd2e
AIYA861202 <phiphiphi27@gmail.com>
2025-10-15 19:19:33 +0800

    測試部屬

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,11 @@
   <div class="container mx-auto p-4">
     <h1 class="text-3xl font-bold mb-4">Nuxt 天氣儀表板</h1>
     <p class="mb-4">歡迎使用 Nuxt 天氣儀表板！這是一個展示 Nuxt.js 功能的簡單應用程式。</p>
-  </div></template>
-<style scoped></style>
+    <NuxtLink to="/" class="link">前往天氣頁面</NuxtLink>
+  </div>
+</template>
+<style lang="scss" scoped>
+.link{
+  color: $main;
+}
+</style>
