fea46d6833dbf9783fb30e2636e0eed6afbaf7fc
AIYA861202 <phiphiphi27@gmail.com>
2025-10-15 19:05:28 +0800

    移除不存在頁面連結

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,5 @@
   <div class="container mx-auto p-4">
     <h1 class="text-3xl font-bold mb-4">Nuxt 天氣儀表板</h1>
     <p class="mb-4">歡迎使用 Nuxt 天氣儀表板！這是一個展示 Nuxt.js 功能的簡單應用程式。</p>
-    <nuxt-link to="/weather" class="text-blue-500 underline">前往天氣頁面</nuxt-link>
   </div></template>
 <style scoped></style>
