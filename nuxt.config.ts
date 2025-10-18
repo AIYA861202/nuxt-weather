8ae9d19cb09a630d136924292df899d68c4d0a43
AIYA861202 <phiphiphi27@gmail.com>
2025-10-18 20:04:05 +0800

    刪去Config public

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   //  ssr: true, // 開啟 SSR 模式
-  css: ['@/assets/styles/main.scss'],
+  css: ["@/assets/styles/main.scss"],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "@/assets/styles/variable.scss";'
-        }
-      }
+          additionalData: '@import "@/assets/styles/variable.scss";',
+        },
+      },
     },
   },
   nitro: {
-    preset: 'github-pages' // 讓 Nuxt 產出 GitHub Pages 相容的靜態檔案
+    preset: "github-pages", // 讓 Nuxt 產出 GitHub Pages 相容的靜態檔案
   },
   app: {
-    baseURL: '/', // 👈 一定要加上你的 repo 名稱（結尾有斜線/nuxt-weather/）
+    baseURL: "/", // 👈 一定要加上你的 repo 名稱（結尾有斜線/nuxt-weather/）
   },
   runtimeConfig: {
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
-    public: {}
   },
-  modules: [
-    '@pinia/nuxt',
-  ],
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  modules: ["@pinia/nuxt"],
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+});
