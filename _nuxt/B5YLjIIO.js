ed6248e5c69260ceccf19350134636a66903e1ab
AIYA861202 <AIYA861202@users.noreply.github.com>
2025-10-18 13:15:41 +0000

    deploy: d916d641d69617442d5c0dda8d27b97728420294

---
+++ _nuxt/B5YLjIIO.js
@@ -0,0 +1,1 @@
+import{_ as s,x as t,y as o,N as c}from"#entry";const r={},a={class:"body"};function n(e,_){return o(),t("div",a,[c(e.$slots,"default")])}const l=s(r,[["render",n]]);export{l as default};
